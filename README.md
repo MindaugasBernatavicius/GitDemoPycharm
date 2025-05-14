c4050d6035561af257aa20b0668c99472d7887f8
Mindaugas <darbas.mindaugas@gmail.com>
2025-05-14 21:02:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# GitDemoPycharm
+
+... 
+
+## Launch
+1. dfsfds
+2. dsfsdf
+3. sdfd
---
+++ main.py
@@ -0,0 +1,7 @@
+import requests
+
+def main():
+    print(requests.get('https://www.delfi.lt'))
+
+if __name__ == '__main__':
+    main()
